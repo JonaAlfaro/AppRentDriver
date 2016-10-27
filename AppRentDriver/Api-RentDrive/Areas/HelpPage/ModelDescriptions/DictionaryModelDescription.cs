cf8cc1149ca161363c83a49a16229a966541bee0
irma__000 <irma__000@IRMA>
2016-10-27 09:40:49 -0500

    Se Creo la Solucion Y el Proyecto del Api

---
+++ AppRentDriver/Api-RentDrive/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Api_RentDrive.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AppRentDriver/Api-RentDrive/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Api_RentDrive.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
