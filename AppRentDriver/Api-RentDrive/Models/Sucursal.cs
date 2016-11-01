65d3b8774b593f09eb13f08f35f256e16254857a
irma__000 <irma__000@IRMA>
2016-11-01 10:07:23 -0600

    Se crearon los modelos de proveedor,reserva,sucursal

---
+++ AppRentDriver/Api-RentDrive/Models/Sucursal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api_RentDrive.Models
+{
+    public class Sucursal
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Calle { get; set; }
+        public string Colonia { get; set; }
+        public string Ciudad { get; set; }
+
+        public virtual ICollection<Reserva> Reserva { get; set; }
+    }
+}
