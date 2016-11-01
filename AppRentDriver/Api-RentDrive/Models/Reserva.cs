f991fb5a7f7c1ce301e6f799324d10a8155a4783
irma__000 <irma__000@IRMA>
2016-11-01 08:20:37 -0600

    Se crearon las models de Proveedor,Reserva,Sucursal.

---
+++ AppRentDriver/Api-RentDrive/Models/Reserva.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api_RentDrive.Models
+{
+    public class Reserva
+    {
+        public int Id { get; set; }
+        public int IdCliente { get; set; }
+        public int IdAutomovil { get; set; }
+        public int IdSucursal { get; set; }
+        public DateTime FechaReserva { get; set; }
+        public Decimal Monto { get; set; }
+
+        public virtual Sucursal Sucursal { get; set; }
+        public virtual Cliente Cliente { get; set; }
+        public virtual Automovil Automovil { get; set; }
+    }
+}
