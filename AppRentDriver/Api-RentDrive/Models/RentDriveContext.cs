df86f19a9c5807658af07a29e46280f7552b179e
armandoomtzz <armandoomtzz@gmail.com>
2016-11-02 05:33:00 -0600

    Creacion de modelos "Automovil", "Cliente" y "RentDriveContext"; modificacion del modelo "Reserva".

---
+++ AppRentDriver/Api-RentDrive/Models/RentDriveContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api_RentDrive.Models
+{
+    public class RentDriveContext : DbContext
+    {
+        public RentDriveContext() : base("RentDriveContext")
+        {
+
+        }
+
+        public DbSet<Automovil> Automoviles { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Proveedor> Proveedores { get; set; }
+        public DbSet<Reserva> Reservaciones { get; set; }
+        public DbSet<Sucursal> Sucursales { get; set; }
+
+        public override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConventions>();
+        }
+    }
+}
--- AppRentDriver/Api-RentDrive/Models/Reserva.cs
+++ AppRentDriver/Api-RentDrive/Models/Reserva.cs
@@ -8,11 +8,12 @@ namespace Api_RentDrive.Models
     public class Reserva
     {
         public int Id { get; set; }
-        public int IdCliente { get; set; }
-        public int IdAutomovil { get; set; }
-        public int IdSucursal { get; set; }
         public DateTime FechaReserva { get; set; }
         public Decimal Monto { get; set; }
+
+        public int ClienteId { get; set; }
+        public int AutomovilId { get; set; }
+        public int SucursalId { get; set; }
 
         public virtual Sucursal Sucursal { get; set; }
         public virtual Cliente Cliente { get; set; }
